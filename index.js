e4b078859af4b18784152ba9f99fd6f2f3d94db9
Ivan Jimenez <chiklete@gmail.com>
2021-10-18 22:49:50 +0200

    Iteration#1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-.
+//Iteración #1: Buscar el máximo
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log('El número más alto es '+numberOne);
+    } else if (numberOne < numberTwo) {
+        console.log('El número más alto es '+numberTwo);
+    } else {
+        console.log('El número '+numberOne + ' y el número '+numberTwo+' son iguales');
+    }
+}
+sum(9,5);
